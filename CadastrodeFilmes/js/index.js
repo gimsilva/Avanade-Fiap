6c722e7eb2ee986581787cc3130479d0b1562a1f
gimsilva <gyduo1@gmail.com>
2022-04-28 15:16:52 -0300

    iniciando os comandos do site no JS

---
+++ CadastrodeFilmes/js/index.js
@@ -0,0 +1,18 @@
+let listaDeFilmes = [
+    'https://br.web.img3.acsta.net/pictures/14/08/11/13/34/537036.jpg', 
+    'https://m.media-amazon.com/images/I/A1JVqNMI7UL._AC_SL1500_.jpg', 
+    'https://br.web.img3.acsta.net/pictures/210/140/21014024_20130619225537406.jpg', 
+    'https://ingresso-a.akamaihd.net/img/cinema/cartaz/6429-cartaz.jpg',
+    'https://i.pinimg.com/originals/28/b9/57/28b95747be06eba5ded65c08ee30f327.jpg',
+    'https://upload.wikimedia.org/wikipedia/pt/d/d4/Ready_Player_One_%28filme%29.png'
+
+]
+
+let listaDeTitulos = [
+    'Relatos Selvagens',
+    'Interstrellar',
+    'Truque de Mestre',
+    'Cantando na Chuva',
+    'Harry Potter e o Prisioneiro de Askban',
+    'Jogador Numero 1'
+]
