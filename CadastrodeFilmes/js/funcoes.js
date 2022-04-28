d8d72856a12c15726f6e098248daedb81faa3377
gimsilva <gyduo1@gmail.com>
2022-04-28 13:43:43 -0300

    iniciando projeto

---
+++ CadastrodeFilmes/js/funcoes.js
@@ -0,0 +1,33 @@
+// let a = Math.floor(Math.random() * 1000)
+// function minhaPrimeiraFuncao(parametros) {
+//   let b = Math.floor(Math.random() * 1000)
+//   if(a > b) {
+//     console.log(`${a} - ${b} = ${a - b}`)
+//   } else {
+//     console.log(`${a} + ${b} = ${a + b}`) 
+//   }
+//   console.log('Estou executando está função!')
+//   return
+// }
+
+// console.logminhaPrimeiraFuncao())og
+
+// const minhaSegundaFuncao = function() {
+//   return 'Escrevendo minha função anônima!'
+// }
+
+// console.log(minhaSegundaFuncao())
+// !esta é uma arrow function
+const a = (nome) => {
+  return 'Está é uma arrow function'
+}
+
+console.log(a('Erik'))
+
+function b(nome) {
+  return 'Esta é uma arrow function'
+}
+
+const c = function(nome) {
+  return 'Está é uma arrow function'
+}
---
+++ CadastrodeFilmes/js/lacos.js
@@ -0,0 +1,24 @@
+// ! Laços de repetição
+
+//* vamos constrir um loop for para fazer as tabuadas de 0 a 10
+
+// let a = 0
+// console.log(a = 0 + 1)
+// console.log(a = a + 1)
+// console.log(a+= 1)
+// console.log( ++a)
+// for(let i = 0; i  <= 10; i++) {
+//     for(let j = 0; j <= 10; j++){
+//         console.log (` A tabuada ${i} * ${j} = ${i * j}`)
+//     }
+// }
+
+//* construindo um loop while
+
+let a = 0
+
+    while (a < 3) { 
+        console.log('Bom dia!')
+        a++
+}
+   
