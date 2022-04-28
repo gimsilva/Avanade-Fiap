ff87f2ea1bf41d30828eac229c289873eb18c280
gimsilva <gyduo1@gmail.com>
2022-04-28 14:48:47 -0300

    modifucando o arquivo

--- CadastrodeFilmes/js/condicionais.js
+++ CadastrodeFilmes/js/condicionais.js
@@ -1,9 +1,21 @@
-let a = Math.floor (Math.random() * 100)
+// let a = Math.floor (Math.random() * 100)
 
-if (a > 50) {
-    console.log( 'O valor de a está na metade maior da sequência')
-} else if ( a == 50) {
-    console.log ('O valor de a é igual a 50')
-} else {
-    console.log('O valor de a esta na metade menor da sequência')
-}
+// if (a > 50) {
+//     console.log( 'O valor de a está na metade maior da sequência')
+// } else if ( a == 50) {
+//     console.log ('O valor de a é igual a 50')
+// } else {
+//     console.log('O valor de a esta na metade menor da sequência')
+// }
+
+// ! fazendo comparação com dois iguais, ou seja, comparação de valores
+
+let a = 50
+let b = 50
+let c = '50'
+
+// console.log(`Esta comparação é: ${a == b}, ou seja, ${a} = ${b}!`)
+// console.log(`Esta comparação é: ${a == b}, ou seja, ${a} = ${c}!`)
+
+console.log(`Esta comparação é: ${a == b}, ou seja, ${a} = ${b}!`)
+console.log(`Esta comparação é: ${a !== b}, ou seja, ${a} !== ${c}!`)
